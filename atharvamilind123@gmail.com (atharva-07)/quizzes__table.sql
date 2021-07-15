e404af39b24145a413fa253a6972b07d179d7d75
Atharva Wankhede <67833926+atharva-07@users.noreply.github.com>
2021-07-15 18:06:21 +0530

    Delete quizzes__table.sql

--- atharvamilind123@gmail.com (atharva-07)/quizzes__table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE quizzes (
-	quiz_id BIGSERIAL PRIMARY KEY,
-	quiz_name VARCHAR(50) NOT NULL,
-	package_id BIGINT NOT NULL,
-	CONSTRAINT fk_quizzes_packages
-		FOREIGN KEY (package_id)
-		REFERENCES packages (package_id)
-);
