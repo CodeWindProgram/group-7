81c7d0b96a8bb5ecc26b263a88e557145315adfe
Atharva Wankhede <67833926+atharva-07@users.noreply.github.com>
2021-07-15 18:05:32 +0530

    Delete question_options__table.sql

--- atharvamilind123@gmail.com (atharva-07)/question_options__table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE question_options (
-	option_id BIGSERIAL PRIMARY KEY,
-	option_text VARCHAR(300) NOT NULL,
-	is_right BOOLEAN NOT NULL DEFAULT FALSE,
-	question_id BIGINT NOT NULL,
-	CONSTRAINT fk_question__options_questions
-		FOREIGN KEY (question_id)
-		REFERENCES questions (question_id)
-);
