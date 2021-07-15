17a06c3b2ab73916c13d47289ff6f52624e63c27
Atharva Wankhede <67833926+atharva-07@users.noreply.github.com>
2021-07-15 18:05:46 +0530

    Delete questions__table.sql

--- atharvamilind123@gmail.com (atharva-07)/questions__table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE questions (
-	question_id BIGSERIAL PRIMARY KEY,
-	question_text VARCHAR(500) NOT NULL,
-	quiz_id BIGINT NOT NULL,
-	CONSTRAINT fk_questions_quizzes
-		FOREIGN KEY (quiz_id)
-		REFERENCES quizzes (quiz_id)
-);
